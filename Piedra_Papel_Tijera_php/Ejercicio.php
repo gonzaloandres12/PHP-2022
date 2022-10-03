c61ab7575e37768986836ed3a5127344e3716411
gonzaloandres12 <114397689+gonzaloandres12@users.noreply.github.com>
2022-10-03 21:14:01 +0200

    Add files via upload

--- Piedra_Papel_Tijera_php/Ejercicio.php
+++ Piedra_Papel_Tijera_php/Ejercicio.php
@@ -5,7 +5,7 @@
     <style>
 
         .game{
-            font-size: 50px;
+            font-size: 60px;
         }
         p{
             font-size: 30px;
@@ -29,6 +29,7 @@
     $jugador2 = random_int(1,3);
     ?>
 
+    <p>Jugador 1 &emsp; Jugador 2</p>
     <div class="game">
         <?php 
         //Asigno el emoticono a un variable con el mismo nombre
@@ -41,66 +42,70 @@
         // 1=>piedara 2=>papel  3=>tijera
         // 1=>piedara 2=>papel  3=>tijera
 
+        //Regular space: &nbsp;
+        //Two spaces gap: &ensp;
+        //Four spaces gap: &emsp;
+
         if($jugador1 == 1 && $jugador2 == 1){
             //piedra y piedra = empate
-            echo "Jugador 1".$piedra1;
-            echo "Jugador 2".$piedra2."<br>";
+            echo "&nbsp;".$piedra1;
+            echo "&emsp;"."&ensp;".$piedra2."<br>";
             echo "<p>Ha sido un empate</p>";
             echo "<p>Recarga la pagina para otra partida</p>";
         }elseif($jugador1 == 2 && $jugador2 == 2){
             //papel y papel = empate
-            echo "Jugador 1".$papel;
-            echo "Jugador 2".$papel."<br>";
+            echo "&nbsp;".$papel;
+            echo "&emsp;"."&ensp;".$papel."<br>";
             echo "<p>Ha sido un empate</p>";
             echo "<p>Recarga la pagina para otra partida</p>";
 
         }elseif($jugador1 == 3 && $jugador2 == 3){
             //tijera y tijera empate
-            echo "Jugador 1".$tijeras;
-            echo "Jugador 2".$tijeras."<br>";
+            echo "&nbsp;".$tijeras;
+            echo "&emsp;"."&ensp;".$tijeras."<br>";
             echo "<p>Ha sido un empate</p>";
             echo "<p>Recarga la pagina para otra partida</p>";
 
         }elseif($jugador1 == 1 && $jugador2==2){
             //piedra y papel = gana j2
-            echo "Jugador 1".$piedra1;
-            echo "Jugador 2".$papel."<br>";
+            echo "&nbsp;".$piedra1;
+            echo "&emsp;"."&ensp;".$papel."<br>";
             echo "<p>Ha ganado el jugador 2</p>";
             echo "<p>Recarga la pagina para otra partida</p>";
         }
         elseif($jugador1 == 1 && $jugador2==3){
             //piedra y tijera = gana j1
-            echo "Jugador 1".$piedra1;
-            echo "Jugador 2".$tijeras."<br>";
+            echo "&nbsp;".$piedra1;
+            echo "&emsp;"."&ensp;".$tijeras."<br>";
             echo "<p>Ha ganado el jugador 1</p>";
             echo "<p>Recarga la pagina para otra partida</p>";
 
         }elseif($jugador1== 2 && $jugador2 == 1){
             //papel y piedra = j1
-            echo "Jugador 1".$papel;
-            echo "Jugador 2".$piedra2."<br>";
+            echo "&nbsp;".$papel;
+            echo "&emsp;"."&ensp;".$piedra2."<br>";
             echo "<p>Ha ganado el jugador 1</p>";
             echo "<p>Recarga la pagina para otra partida</p>";
 
 
         }elseif($jugador1 == 2 && $jugador2==3){
             //papel y tijera = j2
-            echo "Jugador 1".$papel;
-            echo "Jugador 2".$tijeras."<br>";
+            echo "&nbsp;".$papel;
+            echo "&emsp;"."&ensp;".$tijeras."<br>";
             echo "<p>Ha ganado el jugador 2</p>";
             echo "<p>Recarga la pagina para otra partida</p>";
 
 
         }elseif($jugador1 == 3 && $jugador2 == 1){
             //tijera y piedra = j2
-            echo "Jugador 1".$tijeras;
-            echo "Jugador 2".$piedra2."<br>";
+            echo "&nbsp;".$tijeras;
+            echo "&emsp;"."&ensp;".$piedra2."<br>";
             echo "<p>Ha ganado el jugador 2</p>";
             echo "<p>Recarga la pagina para otra partida</p>";
         }elseif($jugador1 == 3 && $jugador2 == 2){
             //tijera  y papel = j1
-            echo "Jugador 1".$tijeras;
-            echo "Jugador 2".$papel."<br>";
+            echo "&nbsp;".$tijeras;
+            echo "&emsp;"."&ensp;".$papel."<br>";
             echo "<p>Ha ganado el jugador 1</p>";
             echo "<p>Recarga la pagina para otra partida</p>";
         }
